590c9a6266870646acf481d34fcb84d6a5668e74
Mateusz Krajewski <maatsior@gmail.com>
2024-03-03 19:16:29 +0100

    add dependency injection core annotations

---
+++ src/main/java/com/github/matsior/summerframework/core/Autowired.java
@@ -0,0 +1,23 @@
+package com.github.matsior.summerframework.core;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used to indicate how to autowire "seeds".
+ * <p>When used on constructor, constructor based dependency injection will be used.
+ * When used on fields, field based dependency injection will be used.
+ * When used on setters, setter dependency injection will be used.
+ *
+ * <p>Note that only one dependency injection strategy can work in class.
+ * Don't mix it in order to avoid unexpected results.
+ *
+ * @author Mateusz Krajewski
+ */
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Autowired {
+
+}
---
+++ src/main/java/com/github/matsior/summerframework/core/Seed.java
@@ -0,0 +1,19 @@
+package com.github.matsior.summerframework.core;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used to mark class as "seed".
+ * Marked classes will be automatically discovered during scanning and
+ * injected to context.
+ *
+ * @author Mateusz Krajewski
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Seed {
+
+}
