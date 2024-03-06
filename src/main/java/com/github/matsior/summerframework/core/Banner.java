d371fded5cdb9e6de856562c71ed2ad05b4e0031
Mateusz Krajewski <maatsior@gmail.com>
2024-03-06 16:07:43 +0100

    add Banner print at application startup

---
+++ src/main/java/com/github/matsior/summerframework/core/Banner.java
@@ -0,0 +1,26 @@
+package com.github.matsior.summerframework.core;
+
+/**
+ * Application banner printed at application startup.
+ *
+ * @author Mateusz Krajewski
+ */
+public class Banner {
+
+  private static final String BANNER = "\n"
+      + "                                          \n"
+      + "  ___ _   _ _ __ ___  _ __ ___   ___ _ __ \n"
+      + " / __| | | | '_ ` _ \\| '_ ` _ \\ / _ \\ '__|\n"
+      + " \\__ \\ |_| | | | | | | | | | | |  __/ |   \n"
+      + " |___/\\__,_|_| |_| |_|_| |_| |_|\\___|_|   \n"
+      + "                                          \n"
+      + "                                          ";
+
+  /**
+   * Prints banner into console.
+   */
+  public void printBanner() {
+    System.out.println(BANNER);
+  }
+
+}
--- src/main/java/com/github/matsior/summerframework/core/SummerApplication.java
+++ src/main/java/com/github/matsior/summerframework/core/SummerApplication.java
@@ -15,6 +15,7 @@ public class SummerApplication {
    */
   public static Context start(Class<?> mainClass)  {
     long start = System.currentTimeMillis();
+    new Banner().printBanner();
     Context context = new Context();
     long end = System.currentTimeMillis();
     System.out.println(mainClass.getSimpleName() + " started successfully. Startup took: " + (end - start) + "ms");
