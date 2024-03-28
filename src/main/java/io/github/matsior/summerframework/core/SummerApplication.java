15207930b3625666ef57aa3e66a1dfaded223f42
Mateusz Krajewski <maatsior@gmail.com>
2024-03-28 21:23:58 +0100

    add WebServer to SummerApplication

--- src/main/java/io/github/matsior/summerframework/core/SummerApplication.java
+++ src/main/java/io/github/matsior/summerframework/core/SummerApplication.java
@@ -1,5 +1,6 @@
 package io.github.matsior.summerframework.core;
 
+import io.github.matsior.summerframework.web.WebServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,8 +21,17 @@ public class SummerApplication {
    */
   public static Context start(Class<?> mainClass)  {
     long start = System.currentTimeMillis();
+
+    Thread mainThread = Thread.currentThread();
+
     new Banner().printBanner();
     Context context = new Context(mainClass);
+
+    WebServer webServer = new WebServer(8080);
+    Thread serverThread = new Thread(webServer);
+    serverThread.start();
+    Runtime.getRuntime().addShutdownHook(new Thread(webServer::shutdown));
+
     long end = System.currentTimeMillis();
     LOGGER.info("{} started successfully. Startup took: {}ms", mainClass.getSimpleName(), (end - start));
     return context;
