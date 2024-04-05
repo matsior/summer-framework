5c38907176b3e562d987000f1640c230cddecd9a
Mateusz Krajewski <maatsior@gmail.com>
2024-04-05 16:21:14 +0200

    add Http documentation

--- src/main/java/io/github/matsior/summerframework/web/Http.java
+++ src/main/java/io/github/matsior/summerframework/web/Http.java
@@ -5,12 +5,27 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks method as HTTP requests handler
+ *
+ * @author Mateusz Krajewski
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Http {
 
+  /**
+   * Indicates what HTTP method should be handling
+   *
+   * @return HttpMethod to handle
+   */
   HttpMethod method();
 
+  /**
+   * Indicates request path to handle
+   *
+   * @return request path
+   */
   String path() default "/";
 
   enum HttpMethod {
