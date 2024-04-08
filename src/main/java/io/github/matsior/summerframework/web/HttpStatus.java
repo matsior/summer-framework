a8f9ff6b9ee1ae5b2ddcbac8c74d096cd2b8c37b
Mateusz Krajewski <maatsior@gmail.com>
2024-04-08 23:49:59 +0200

    add statusCode

--- src/main/java/io/github/matsior/summerframework/web/HttpStatus.java
+++ src/main/java/io/github/matsior/summerframework/web/HttpStatus.java
@@ -1,8 +1,15 @@
 package io.github.matsior.summerframework.web;
 
 public enum HttpStatus {
-  OK,
-  NO_CONTENT,
-  BAD_REQUEST,
-  NOT_FOUND
+  OK(200),
+  NO_CONTENT(204),
+  BAD_REQUEST(400),
+  NOT_FOUND(404);
+
+  private final int statusCode;
+
+  HttpStatus(int statusCode) {
+    this.statusCode = statusCode;
+  }
+
 }
