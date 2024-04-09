4221921c1fe9785c099b8bdc5cf6b368de9c6c2e
Mateusz Krajewski <maatsior@gmail.com>
2024-04-09 22:01:32 +0200

    add HttpStatus description

--- src/main/java/io/github/matsior/summerframework/web/HttpStatus.java
+++ src/main/java/io/github/matsior/summerframework/web/HttpStatus.java
@@ -1,15 +1,17 @@
 package io.github.matsior.summerframework.web;
 
 public enum HttpStatus {
-  OK(200),
-  NO_CONTENT(204),
-  BAD_REQUEST(400),
-  NOT_FOUND(404);
+  OK(200, "OK"),
+  NO_CONTENT(204, "No Content"),
+  BAD_REQUEST(400, "Bad Request"),
+  NOT_FOUND(404, "Not Found");
 
   private final int statusCode;
+  private final String description;
 
-  HttpStatus(int statusCode) {
+  HttpStatus(int statusCode, String description) {
     this.statusCode = statusCode;
+    this.description = description;
   }
 
 }
